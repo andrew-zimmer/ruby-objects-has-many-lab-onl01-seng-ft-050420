6a1af8c883d0115b4ce1075173320bebc1009f27
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-18 14:25:34 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+class Song 
+  @@all = []
+  attr_accessor :name, :genre, :artist
+  def initialize(name)
+    @name = name 
+    #@genre = genre 
+    @@all << self 
+  end 
+  def self.all 
+    @@all 
+  end 
+end 
