bc8b0b5d0c0e8db3b65f03909e3599a7d46f3d30
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-18 14:27:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,5 +10,8 @@ class Artist
   def add_song(song)
     song.artist = self
   end 
-  def add_song_by_name(name, genre)
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.add_song(song)
+  end 
 end 
