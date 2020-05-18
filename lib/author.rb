978888484af0b6f84170e37ac3608fba040fc240
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-18 14:41:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,8 +2,12 @@ class Author
   attr_accessor :name 
   def initialize(name)
     @name = name 
+    @post = []
   end 
   def posts 
     Post.all.select {|post| post.author == self}
+  end 
+  def add_post(title)
+    @post << title
   end 
 end 
